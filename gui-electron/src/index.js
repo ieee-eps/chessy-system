523affae5ef7a65956398cc68497b673145954b6
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-11-20 21:56:47 +0100

    Changement de l'indexé

--- gui-electron/src/index.js
+++ gui-electron/src/index.js
@@ -9,18 +9,19 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    frame:false,
+    titleBarStyle: "hidden",
+    fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, '../../interface/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../../interface/interface.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
